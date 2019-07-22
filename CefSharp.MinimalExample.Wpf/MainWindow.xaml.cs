9935b8cc137fa2fdfc60be4036704adee3f44d5c
Igor <inputoutput@inbox.ru>
2019-07-22 16:53:06 +0500

    Reproduce #2824

--- CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -1,12 +1,35 @@
 ﻿using System.Windows;
+using CefSharp.Wpf;
 
 namespace CefSharp.MinimalExample.Wpf
 {
     public partial class MainWindow : Window
     {
+        private readonly ChromiumWebBrowser _browser = new ChromiumWebBrowser("http://google.com");
+
         public MainWindow()
         {
             InitializeComponent();
+
+            Border.Child = _browser;
+        }
+
+        private void OnOpenWindowButtonClick(object sender, RoutedEventArgs e)
+        {
+            Border.Child = null;
+
+            var floatingWindow = new FloatingWindow
+            {
+                Content = _browser
+            };
+
+            floatingWindow.Closed += (o, args) =>
+            {
+                floatingWindow.Content = null;
+                Border.Child = _browser;
+            };
+
+            floatingWindow.Show();
         }
     }
 }
